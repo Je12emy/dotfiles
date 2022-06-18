6bd4970e988ccc14cea8c95a726d38ff5f3140aa
Jeremy <jeremy@je12emy.com>
2022-06-18 01:20:35 -0600

    refactor(nvim): Update nvimtree keybinds)

--- nvim/lua/plugins/nvimtree.lua
+++ nvim/lua/plugins/nvimtree.lua
@@ -1,4 +1,26 @@
+-- Vim-like mappings I guess
+local mappings = {
+    { key = "o", action = "edit" },
+    { key = "l", action = "cd" },
+    { key = "h", action = "dir_up" },
+    { key = "K", action = "toggle_file_info" },
+    { key = "r", action = "rename" },
+    { key = "a", action = "create" },
+    { key = "d", action = "remove" },
+    { key = "I", action = "toggle_git_ignored" },
+    { key = "H", action = "toggle_dotfiles" },
+    { key = "R", action = "refresh" },
+    { key = "y", action = "copy_name" },
+    { key = "Y", action = "copy_path" },
+    { key = "gy", action = "copy_absolute_path" }
+}
+
 require'nvim-tree'.setup {
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+  },
+  hijack_cursor = true,
   filters = {
     dotfiles = true,
     custom = { ".git" }
@@ -6,6 +28,10 @@ require'nvim-tree'.setup {
   view = {
     width = '15%',
     side = 'right',
+    mappings = {
+      custom_only = true,
+      list = mappings,
+    },
   }
 }
 
