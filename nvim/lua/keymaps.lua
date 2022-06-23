b197cc8aada54173464daeedc2d4a440509aa7bb
Jeremy <jeremy@je12emy.com>
2022-06-23 16:51:30 -0600

    feat(nvim): add new tab management keymaps
    Article: https://medium.com/@paulodiovani/vim-buffers-windows-and-tabs-an-overview-8e2a57c57afa

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -2,13 +2,13 @@
 vim.keymap.set('i', 'jk', '<Esc>')
 vim.keymap.set('n', 'S', ':%s//g<Left><Left>')
 
----- Navigation Keymaps
+-- Window Managament
+---- Navigation 
 vim.keymap.set('n', '<c-h>', '<cmd>wincmd h<CR>')
 vim.keymap.set('n', '<c-j>', '<cmd>wincmd j<CR>')
 vim.keymap.set('n', '<c-l>', '<cmd>wincmd l<CR>')
 vim.keymap.set('n', '<c-k>', '<cmd>wincmd k<CR>')
-
--- Window Split Manipulation
+---- Splits
 vim.keymap.set( 'n',   '<Space>wv', '<C-w>v<CR>')
 vim.keymap.set( 'n',   '<Space>ws', '<C-w>s<CR>')
 vim.keymap.set( 'n',   '<Space>wh', ':exe "vertical resize -" .5<CR>')
@@ -16,6 +16,15 @@ vim.keymap.set( 'n',   '<Space>wl', ':exe "vertical resize +" .5<CR>')
 vim.keymap.set( 'n',   '<Space>wj', ':exe "resize -" .5<CR>')
 vim.keymap.set( 'n',   '<Space>wk', ':exe "resize +" .5<CR>')
 vim.keymap.set( 'n',   '<Space>wr', '<C-w>=<CR>')
+
+-- Buffer Management
+vim.keymap.set( 'n',   '<a-}>', '<cmd>bnext<CR>')
+vim.keymap.set( 'n',   '<a-{>', '<cmd>bprev<CR>')
+vim.keymap.set( 'n',   '<a-x>', '<cmd>bdelete<CR>')
+
+-- Tab Management
+vim.keymap.set( 'n',   '<Space>tn', '<cmd>tabnew<CR>')
+vim.keymap.set( 'n',   '<Space>tx', '<cmd>tabclose<CR>')
 
 -- Writting
 vim.keymap.set( 'n',   '<F8>', ':setlocal spell!<CR>')
