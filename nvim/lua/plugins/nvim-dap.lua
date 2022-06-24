e20675649f8a3895bd39038969a242dacfad37ec
Jeremy <jeremy@je12emy.com>
2022-06-24 11:16:12 -0600

    feat(nvim): update the rust tools dap configuration and nvim-dap

--- nvim/lua/plugins/nvim-dap.lua
+++ nvim/lua/plugins/nvim-dap.lua
@@ -1,5 +1,6 @@
-local dap = require('dap')
+local dap, dapui = require("dap"), require("dapui")
 
+vim.keymap.set('n', '<a-d>', dapui.toggle)
 vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint)
 vim.keymap.set('n', '<leader>B', "lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
 vim.keymap.set('n', '<F1>', dap.step_back)
@@ -8,38 +9,36 @@ vim.keymap.set('n', '<F3>', dap.step_over)
 vim.keymap.set('n', '<F4>', dap.step_out)
 vim.keymap.set('n', '<F5>', dap.continue)
 
--- Go Setup
-dap.adapters.go = {
-  type = 'executable';
-  command = 'node';
-  args = {os.getenv('HOME') .. '/source/vscode-go/dist/debugAdapter.js'};
-}
-dap.configurations.go = {
-  {
-    type = 'go';
-    name = 'Debug';
-    request = 'launch';
-    showLog = false;
-    program = "${file}";
-    dlvToolPath = vim.fn.exepath('dlv')  -- Adjust to where delve is installed
-  },
-}
-
-require('dapui').setup()
-
-local dap, dapui = require("dap"), require("dapui")
 dap.listeners.after.event_initialized["dapui_config"] = function()
   dapui.open()
 end
+
 dap.listeners.before.event_terminated["dapui_config"] = function()
   dapui.close()
 end
+
 dap.listeners.before.event_exited["dapui_config"] = function()
   dapui.close()
 end
 
-vim.keymap.set('n', '<a-d>', dapui.toggle)
+-- I need to reconfigure for Go
+-- Go Setup
+-- dap.adapters.go = {
+--   type = 'executable';
+--   command = 'node';
+--   args = {os.getenv('HOME') .. '/source/vscode-go/dist/debugAdapter.js'};
+-- }
+-- dap.configurations.go = {
+--   {
+--     type = 'go';
+--     name = 'Debug';
+--     request = 'launch';
+--     showLog = false;
+--     program = "${file}";
+--     dlvToolPath = vim.fn.exepath('dlv')  -- Adjust to where delve is installed
+--   },
+-- }
 
-local dapgo = require('dap-go')
-dapgo.setup()
-vim.keymap.set('n', '<leader>ddt', dapgo.debug_test)
+-- local dapgo = require('dap-go')
+-- dapgo.setup()
+-- vim.keymap.set('n', '<leader>ddt', dapgo.debug_test)
--- nvim/lua/plugins/rust-tools.lua
+++ nvim/lua/plugins/rust-tools.lua
@@ -3,6 +3,13 @@ local tools = require 'utils/lsp_tools'
 local capabilities = tools.get_capabilities()
 
 require('rust-tools').setup({
+        dap = {
+            adapter = {
+                type = 'executable',
+                command = 'lldb-vscode-14', -- sudo apt-get install lldb
+                name = "rt_lldb"
+            }
+        },
         server = {
             on_attach = tools.on_attach,
             capabilities = capabilities,
