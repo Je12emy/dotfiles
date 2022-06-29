7bb1cf608519a642ccde7d933cb9014444e6a257
Jeremy <jeremy@je12emy.com>
2022-06-29 09:46:59 -0600

    feat(nvim): remove the plugin rooter-nvim and update nvimtree
    Removed the usage of the rooter-nvim plugin and updated the nvimtree
    configuration to disable it's integration with rooter-nvim

--- nvim/lua/plugins/nvimtree.lua
+++ nvim/lua/plugins/nvimtree.lua
@@ -17,10 +17,6 @@ local mappings = {
 
 require'nvim-tree'.setup {
   update_cwd = true,
-  update_focused_file = {
-    enable = true,
-    update_cwd = true
-  },
   diagnostics = {
     enable = true,
     show_on_dirs = true,
