360d13983a179f14bd1a649c96493f3c112c7fd9
Jeremy <jeremy@je12emy.com>
2022-08-27 16:07:44 -0600

    fix(awesome): remove spawn with shell duplication

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -183,50 +183,50 @@ awful.screen.connect_for_each_screen(function(s)
               awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
-    -- s.mypromptbox = awful.widget.prompt()
+    s.mypromptbox = awful.widget.prompt()
     -- Create an imagebox widget which will contain an icon indicating which layout we're using.
     -- We need one layoutbox per screen.
-    -- s.mylayoutbox = awful.widget.layoutbox(s)
-    -- s.mylayoutbox:buttons(gears.table.join(
-    --                        awful.button({ }, 1, function () awful.layout.inc( 1) end),
-    --                        awful.button({ }, 3, function () awful.layout.inc(-1) end),
-    --                        awful.button({ }, 4, function () awful.layout.inc( 1) end),
-    --                        awful.button({ }, 5, function () awful.layout.inc(-1) end)))
+    s.mylayoutbox = awful.widget.layoutbox(s)
+    s.mylayoutbox:buttons(gears.table.join(
+                           awful.button({ }, 1, function () awful.layout.inc( 1) end),
+                           awful.button({ }, 3, function () awful.layout.inc(-1) end),
+                           awful.button({ }, 4, function () awful.layout.inc( 1) end),
+                           awful.button({ }, 5, function () awful.layout.inc(-1) end)))
     -- Create a taglist widget
-    -- s.mytaglist = awful.widget.taglist {
-    --     screen  = s,
-    --     filter  = awful.widget.taglist.filter.all,
-    --     buttons = taglist_buttons
-    -- }
+    s.mytaglist = awful.widget.taglist {
+        screen  = s,
+        filter  = awful.widget.taglist.filter.all,
+        buttons = taglist_buttons
+    }
 
     -- Create a tasklist widget
-    -- s.mytasklist = awful.widget.tasklist {
-    --     screen  = s,
-    --     filter  = awful.widget.tasklist.filter.currenttags,
-    --     buttons = tasklist_buttons
-    -- }
+    s.mytasklist = awful.widget.tasklist {
+        screen  = s,
+        filter  = awful.widget.tasklist.filter.currenttags,
+        buttons = tasklist_buttons
+    }
 
     -- Create the wibox
-    -- s.mywibox = awful.wibar({ position = "top", screen = s })
+    s.mywibox = awful.wibar({ position = "top", screen = s })
 
     -- Add widgets to the wibox
-    -- s.mywibox:setup {
-    --     layout = wibox.layout.align.horizontal,
-    --     { -- Left widgets
-    --         layout = wibox.layout.fixed.horizontal,
-    --         mylauncher,
-    --         s.mytaglist,
-    --         s.mypromptbox,
-    --     },
-    --     s.mytasklist, -- Middle widget
-    --     { -- Right widgets
-    --         layout = wibox.layout.fixed.horizontal,
-    --         mykeyboardlayout,
-    --         wibox.widget.systray(),
-    --         mytextclock,
-    --         s.mylayoutbox,
-    --     },
-    -- }
+     s.mywibox:setup {
+         layout = wibox.layout.align.horizontal,
+         { -- Left widgets
+             layout = wibox.layout.fixed.horizontal,
+             mylauncher,
+             s.mytaglist,
+             s.mypromptbox,
+         },
+         s.mytasklist, -- Middle widget
+         { -- Right widgets
+             layout = wibox.layout.fixed.horizontal,
+             mykeyboardlayout,
+             wibox.widget.systray(),
+             mytextclock,
+             s.mylayoutbox,
+         },
+     }
 end)
 -- }}}
 
@@ -575,5 +575,6 @@ client.connect_signal("unfocus",
 awful.spawn.with_shell("monitors.sh");
 awful.spawn.with_shell("leds.sh");
 awful.spawn.with_shell("nitrogen --restore");
-awful.spawn.with_shell("flatpak run com.nextcloud.desktopclient.nextcloud");
+awful.spawn.single_instance("flatpak run com.nextcloud.desktopclient.nextcloud");
+awful.spawn.single_instance("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &");
 -- awful.spawn.with_shell("wmname LG3D");
