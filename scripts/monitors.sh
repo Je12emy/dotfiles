36950b468ab3cf0b0845ee28df7785114ade6ffe
je12emy <jeremy@je12emy.com>
2023-01-18 20:45:30 -0600

    fix(scripts): change monitor set-up

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -1,2 +1,6 @@
 #! /bin/bash
-xrandr --output DisplayPort-2 --mode 1360x768 --rate 60.00 --output HDMI-A-0 --primary --mode 1920x1080 --rate 144.01 --left-of DisplayPort-2
+# HDMI-A-0 is my primary monitor
+# DisplayPort-2 is my secondary monitor
+xrandr \
+	--output DisplayPort-2 --mode 1360x768 --rate 60.00 \
+	--output HDMI-A-0 --primary --mode 1920x1080 --rate 144.01 --left-of DisplayPort-2 \
