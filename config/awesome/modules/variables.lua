24e2ed9d3a1f96f00c287c849c7342d55948dc02
Jeremy <jeremy@je12emy.com>
2023-03-20 01:11:52 -0600

    Revert "refactor(awesome): extract global variables"
    This reverts commit e5a11a2ea6c8cc2aecb1a79381a29d091a33a2a4.

--- config/awesome/modules/variables.lua
+++
@@ -1,9 +0,0 @@
-local beautiful = require("beautiful")
--- This is used later as the default terminal and editor to run.
-terminal = "kitty"
-editor = os.getenv("EDITOR") or "editor"
-editor_cmd = terminal .. " -e " .. editor
-
-local theme_path = string.format("%s/.config/awesome/themes/%s/theme.lua",
-    os.getenv("HOME"), "catpuccin")
-beautiful.init(theme_path)
