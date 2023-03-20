f99128b5462ed8d7905851aee216035ea3ba888a
Jeremy <jeremy@je12emy.com>
2023-03-19 23:11:46 -0600

    feat(awesome): update 2nd monitor resolution

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -2,5 +2,5 @@
 # HDMI-A-0 is my primary monitor
 # DisplayPort-2 is my secondary monitor
 xrandr \
-	--output DisplayPort-2 --mode 1360x768 --rate 60.00 \
+	--output DisplayPort-2 --mode 1920x1080 --rate 60.00 \
 	--output HDMI-A-0 --primary --mode 1920x1080 --rate 144.01 --left-of DisplayPort-2 \
