8f48c5c54528764282e9f2a17ca58bad09bf22d6
Jeremy <jeremy@je12emy.com>
2023-04-04 14:07:05 -0600

    feat(nvim): setup netrw.nvim in lazy

--- config/nvim/lua/je12emy/plugins/ui.lua
+++ config/nvim/lua/je12emy/plugins/ui.lua
@@ -1,1 +1,20 @@
-return {"hoob3rt/lualine.nvim", "mkitt/tabline.vim", "prichrd/netrw.nvim"}
+return {
+    {"hoob3rt/lualine.nvim", lazy = true}, {"mkitt/tabline.vim", lazy = true}, {
+        "prichrd/netrw.nvim",
+        lazy = false,
+        cmd = {"Ex", "Sex"},
+        keys = {{"<a-e>, vim.cmd.Ex", desc = "Open Netrw"}},
+        config = function()
+            require("netrw").setup {
+                use_devicons = true -- Uses nvim-web-devicons if true, otherwise use the file icon specified above
+            }
+            local g = vim.g
+            g.netrw_banner = 0
+            -- 1 - open files in a new horizontal split
+            -- 2 - open files in a new vertical split
+            -- 3 - open files in a new tab
+            -- 4 - open in previous window
+            g.netrw_browse_split = 0
+        end
+    }
+}
