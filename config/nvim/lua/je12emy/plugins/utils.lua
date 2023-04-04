e718abc48b744960d540da1a639c9d38eb1ae398
Jeremy <jeremy@je12emy.com>
2023-04-04 14:16:54 -0600

    feat(nvim): setup comment with lazy

--- config/nvim/lua/je12emy/plugins/utils.lua
+++ config/nvim/lua/je12emy/plugins/utils.lua
@@ -1,6 +1,17 @@
 return {
-    "tpope/vim-surround", "numToStr/Comment.nvim",
-    {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
+    "tpope/vim-surround", {
+        "numToStr/Comment.nvim",
+        opts = {
+            padding = true,
+            sticky = true,
+            ignore = nil,
+            toggler = {line = 'gcc', block = 'gbc'},
+            opleader = {line = 'gc', block = 'gb'},
+            mappings = {basic = true, extra = true, extended = false},
+            pre_hook = nil,
+            post_hook = nil
+        }
+    }, {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
     "nvim-treesitter/playground", "nvim-treesitter/nvim-treesitter-context",
     "alexghergh/nvim-tmux-navigation", "ThePrimeagen/harpoon",
     -- "github/copilot.vim",
