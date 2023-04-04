5aeb5803554c0b9d1d744ddecbd23740667ae63e
Jeremy <jeremy@je12emy.com>
2023-04-03 22:53:05 -0600

    feat(nvim): add lazy bootstrap

--- config/nvim/lua/je12emy/plugins.lua
+++ config/nvim/lua/je12emy/plugins.lua
@@ -1,12 +1,17 @@
-local fn = vim.fn
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({
-        'git', 'clone', '--depth', '1',
-        'https://github.com/wbthomason/packer.nvim', install_path
-    })
-    vim.cmd 'packadd packer.nvim'
+-- Bootstrap Lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
+
 -- Plugins list
 require('packer').startup(function(use)
     -- Packer can manage itself
