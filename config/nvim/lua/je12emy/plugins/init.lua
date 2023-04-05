a6cd001e0502034dba92fb910772bf4e82a76c5d
Jeremy <jeremy@je12emy.com>
2023-04-04 22:48:27 -0600

    feat(nvim): configure lualine with lazy

--- config/nvim/lua/je12emy/plugins/init.lua
+++ config/nvim/lua/je12emy/plugins/init.lua
@@ -1,21 +1,11 @@
 return {
     -- -- LSP
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    -- Completition
-    "L3MON4D3/LuaSnip",
-    "rafamadriz/friendly-snippets",
-    -- Completition
-    "hrsh7th/nvim-cmp",
-    "onsails/lspkind-nvim",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-nvim-lua",
-    "hrsh7th/cmp-path",
-    "saadparwaiz1/cmp_luasnip",
-    -- Language Specific Plugins
-    "simrat39/rust-tools.nvim",
-    "jose-elias-alvarez/typescript.nvim",
-    "wuelnerdotexe/vim-astro",
+    "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig", -- Completition
+    "L3MON4D3/LuaSnip", "rafamadriz/friendly-snippets", -- Completition
+    "hrsh7th/nvim-cmp", "onsails/lspkind-nvim", "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lua", "hrsh7th/cmp-path",
+    "saadparwaiz1/cmp_luasnip", -- Language Specific Plugins
+    "simrat39/rust-tools.nvim", "jose-elias-alvarez/typescript.nvim",
+    "wuelnerdotexe/vim-astro"
 }
