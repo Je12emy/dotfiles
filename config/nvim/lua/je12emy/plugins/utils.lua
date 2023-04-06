0602b8f7925125451096bf0205fdf855aa9c0e9d
Jeremy <jeremy@je12emy.com>
2023-04-06 17:59:38 -0600

    feat(nvim): configure tmux nav with lazy

--- config/nvim/lua/je12emy/plugins/utils.lua
+++ config/nvim/lua/je12emy/plugins/utils.lua
@@ -21,7 +21,36 @@ return {
             context.setup {}
         end
     }, "nvim-treesitter/playground", "nvim-treesitter/nvim-treesitter-context",
-    "alexghergh/nvim-tmux-navigation", -- "github/copilot.vim",
+    {
+        "alexghergh/nvim-tmux-navigation",
+        lazy = true,
+        opts = {
+            disable_when_zoomed = true
+        },
+        keys = {
+            {
+                "<c-h>",
+                "<cmd>NvimTmuxNavigateLeft<cr>",
+                desc = "Focus left"
+            },
+            {
+                "<c-l>",
+                "<cmd>NvimTmuxNavigateRight<cr>",
+                desc = "Focus right"
+            },
+            {
+                "<c-j>",
+                "<cmd>NvimTmuxNavigateDown<cr>",
+                desc = "Focus down"
+            },
+            {
+                "<c-k>",
+                "<cmd>NvimTmuxNavigateUp<cr>",
+                desc = "Focus up"
+            },
+        },
+        cmd = {"NvimTmuxNavigateLeft","NvimTmuxNavigateRight", "NvimTmuxNavigateUp", "NvimTmuxNavigateDown"}
+    }, -- "github/copilot.vim",
     {
         "sbdchd/neoformat",
         cmd = "Neoformat",
