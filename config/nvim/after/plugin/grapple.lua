5fd42969294fb6a44c79542c13cb217b1ed32016
Jeremy <jeremy@je12emy.com>
2023-04-07 02:11:03 -0600

    refactor(nvim): remove unsued code

--- config/nvim/after/plugin/grapple.lua
+++
@@ -1,6 +0,0 @@
-local ok, grapple = pcall(require, 'grapple')
-if not ok then return end
-
-grapple.setup({
-
-})
--- config/nvim/after/plugin/gruvbox.lua
+++
@@ -1,9 +0,0 @@
-local ok, gruvbox = pcall(require, 'gruvbox')
-if not ok then return end
-gruvbox.setup({
-    contrast = "hard", -- can be "hard", "soft" or empty string
-    palette_overrides = {
-        dark0_hard = "#1c2021",
-    }
-})
-vim.o.background = "dark"
