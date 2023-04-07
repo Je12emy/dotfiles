1be4c13fc1f8dfee2fa7f896b6c53f5fc49e64d7
Jeremy <jeremy@je12emy.com>
2023-04-06 18:14:20 -0600

    feat(nvim): configure language plugins with lazy

--- config/nvim/lua/je12emy/plugins/init.lua
+++ config/nvim/lua/je12emy/plugins/init.lua
@@ -6,6 +6,4 @@ return {
     "hrsh7th/nvim-cmp", "onsails/lspkind-nvim", "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lua", "hrsh7th/cmp-path",
     "saadparwaiz1/cmp_luasnip", -- Language Specific Plugins
-    "simrat39/rust-tools.nvim", "jose-elias-alvarez/typescript.nvim",
-    "wuelnerdotexe/vim-astro"
 }
