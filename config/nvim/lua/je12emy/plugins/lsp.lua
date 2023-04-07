ca18e05522c25753ad6e678e664e6ab4eb36c42e
Jeremy <jeremy@je12emy.com>
2023-04-07 00:19:05 -0600

    feat(nvim): configure snippets

--- config/nvim/lua/je12emy/plugins/lsp.lua
+++ config/nvim/lua/je12emy/plugins/lsp.lua
@@ -3,38 +3,52 @@ return {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
         dependencies = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'}, -- Required
-            { -- Optional
+            { 'neovim/nvim-lspconfig' },
+            {
+                -- Optional
                 'williamboman/mason.nvim',
                 build = function() pcall(vim.cmd, 'MasonUpdate') end
-            }, {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            }, { 'williamboman/mason-lspconfig.nvim' },
             -- Autocompletion
             {
                 'hrsh7th/nvim-cmp',
-                dependencies = {"onsails/lspkind.nvim"}
-            }, -- Required
-            {'hrsh7th/cmp-nvim-lsp'}, -- Required
-            {'L3MON4D3/LuaSnip'} -- Required
+                dependencies = {
+                    "onsails/lspkind.nvim", "rafamadriz/friendly-snippets"
+                }
+            },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'L3MON4D3/LuaSnip' }
+
         },
         config = function()
             local lsp = require('lsp-zero').preset({})
             local tools = require 'je12emy.lsp.lsp-tools'
 
-            lsp.on_attach(tools.on_attach)
+            lsp.on_attach(function(client, bufnr)
+                tools.on_attach(client, bufnr)
+                lsp.buffer_autoformat()
+            end)
 
             require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
             lsp.setup()
 
             local cmp = require('cmp')
+            require('luasnip.loaders.from_vscode').lazy_load()
+
             cmp.setup({
+                completion = { autocomplete = false },
+                mapping = {
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                },
+                sources = { { name = 'nvim_lsp' }, { name = 'luasnip' } },
                 formatting = {
-                    fields = {'abbr', 'kind', 'menu'},
+                    fields = { 'abbr', 'kind', 'menu' },
                     format = require('lspkind').cmp_format({
-                        mode = 'symbol_text', -- show only symbol annotations
+                        mode = 'symbol_text',
                         maxwidth = 50, -- prevent the popup from showing more than provided characters
-                        ellipsis_char = '...' -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
+                        ellipsis_char =
+                        '...'          -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
                     })
                 }
             })
