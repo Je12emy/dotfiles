801132ebe273e726f259225cb4f44ee041f8005c
Jeremy <jeremy@je12emy.com>
2023-04-07 00:09:30 -0600

    feat(nvim): configure lsp-kind

--- config/nvim/lua/je12emy/plugins/lsp.lua
+++ config/nvim/lua/je12emy/plugins/lsp.lua
@@ -10,7 +10,10 @@ return {
                 build = function() pcall(vim.cmd, 'MasonUpdate') end
             }, {'williamboman/mason-lspconfig.nvim'}, -- Optional
             -- Autocompletion
-            {'hrsh7th/nvim-cmp'}, -- Required
+            {
+                'hrsh7th/nvim-cmp',
+                dependencies = {"onsails/lspkind.nvim"}
+            }, -- Required
             {'hrsh7th/cmp-nvim-lsp'}, -- Required
             {'L3MON4D3/LuaSnip'} -- Required
         },
@@ -23,6 +26,18 @@ return {
             require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
             lsp.setup()
+
+            local cmp = require('cmp')
+            cmp.setup({
+                formatting = {
+                    fields = {'abbr', 'kind', 'menu'},
+                    format = require('lspkind').cmp_format({
+                        mode = 'symbol_text', -- show only symbol annotations
+                        maxwidth = 50, -- prevent the popup from showing more than provided characters
+                        ellipsis_char = '...' -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
+                    })
+                }
+            })
         end
     }
 }
