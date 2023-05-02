2cf7782e9efc90ae4cde57ef2404485e08685392
Jeremy <jeremy@je12emy.com>
2023-05-01 22:47:39 -0600

    feat(nvim): remove lualine, use my own statusline

--- config/nvim/lua/je12emy/plugins/colorscheme.lua
+++ config/nvim/lua/je12emy/plugins/colorscheme.lua
@@ -7,7 +7,14 @@ return {
         config = function(_, opts)
             vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
             vim.cmd.colorscheme("catppuccin")
+            require("catppuccin").setup {
+                custom_highlights = function(colors)
+                    return {
+                        StatusLine = { bg = colors.none },
+                    }
+                end
+            }
         end
-    }, {"ellisonleao/gruvbox.nvim", lazy = true},
-    {"rebelot/kanagawa.nvim", lazy = true}
+    }, { "ellisonleao/gruvbox.nvim", lazy = true },
+    { "rebelot/kanagawa.nvim",    lazy = true }
 }
