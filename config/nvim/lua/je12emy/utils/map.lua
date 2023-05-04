c4fffc330be552099b666c41db49aaccc5bb004a
Jeremy <jeremy@je12emy.com>
2023-05-04 16:36:01 -0600

    feat(nvim): update lsp keymaps

--- config/nvim/lua/je12emy/utils/map.lua
+++ config/nvim/lua/je12emy/utils/map.lua
@@ -1,9 +1,11 @@
 local M = {}
 
+-- Shorthand for setting keymaps on "normal" mode
 M.nmap = function(l, r, opts)
     vim.keymap.set("n", l, r, opts)
 end
 
+-- Shorthand for setting keymaps on "insert" mode
 M.imap = function(l, r, opts)
     vim.keymap.set("i", l, r, opts)
 end
