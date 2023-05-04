ce5039bc98b4bb2d515df369333310fe6d49a059
Jeremy <jeremy@je12emy.com>
2023-05-04 16:36:01 -0600

    feat(nvim): update lsp keymaps

--- config/nvim/lua/je12emy/utils/lsp-tools.lua
+++ config/nvim/lua/je12emy/utils/lsp-tools.lua
@@ -1,22 +1,27 @@
-local tools = {}
+local keymap = require "je12emy.utils.map"
 
-function tools.on_attach(client, bufnr)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover,
+local M = {}
+
+-- Callback for setting all my LSP keymaps
+M.on_attach = function(client, bufnr)
+    keymap.nmap("K", vim.lsp.buf.hover,
         { buffer = 0, desc = "Show documentation" })
-    vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename,
+    keymap.nmap("<leader>cr", vim.lsp.buf.rename,
         { buffer = 0, desc = "Rename symbol" })
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action,
+    keymap.nmap("<leader>ca", vim.lsp.buf.code_action,
         { buffer = 0, desc = "Show code actions" })
-    vim.keymap.set("n", "<leader>ci", vim.lsp.buf.implementation,
+    keymap.nmap("<leader>ci", vim.lsp.buf.implementation,
         { buffer = 0, desc = "Show implementations" })
-    vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition,
+    keymap.nmap("<leader>cd", vim.lsp.buf.definition,
         { buffer = 0, desc = "Show definitions" })
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev,
+    keymap.nmap("[d", vim.diagnostic.goto_prev,
         { buffer = 0, desc = "Go to previous diagnostic" })
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_next,
+    keymap.nmap("]d", vim.diagnostic.goto_next,
         { buffer = 0, desc = "Go to next diagnostic" })
-    vim.keymap.set("n", "<leader><leader>f", vim.lsp.buf.format,
+    keymap.nmap("<leader><leader>f", vim.lsp.buf.format,
         { buffer = 0, desc = "Format buffer" })
+    keymap.imap("<c-k>", vim.lsp.buf.signature_help,
+        { buffer = 0, desc = "Show completition item" })
 end
 
-return tools
+return M
