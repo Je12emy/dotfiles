0f19e9097fee9b9862f8b649c8b94e500c3e3cc1
Jeremy <jeremy@je12emy.com>
2023-05-05 17:31:28 -0600

    feat(nvim): reduce rust tools set-up

--- config/nvim/lua/je12emy/plugins/lsp.lua
+++ config/nvim/lua/je12emy/plugins/lsp.lua
@@ -50,12 +50,8 @@ return {
             local rust_tools = require('rust-tools')
             rust_tools.setup({
                 server = {
-                    on_attach = function()
-                        vim.keymap.set('n', '<leader>ca',
-                            rust_tools.hover_actions.hover_actions, {
-                                buffer = bufnr,
-                                desc = "Show hover actions with rust tools"
-                            })
+                    on_attach = function(client, bufnr)
+                        tools.on_attach(client, bufnr)
                     end
                 }
             })
