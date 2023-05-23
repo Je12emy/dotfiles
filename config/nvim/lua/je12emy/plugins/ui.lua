0a79c2213860e135efe2783305ae6b50f966097b
Jeremy <jeremy@je12emy.com>
2023-05-22 23:14:01 -0600

    fix(nvim): load oil.nvim on start-up

--- config/nvim/lua/je12emy/plugins/ui.lua
+++ config/nvim/lua/je12emy/plugins/ui.lua
@@ -22,25 +22,31 @@ return {
     },
     {
         'stevearc/oil.nvim',
-        opts = {
-            keymaps = {
-                ["g?"] = "actions.show_help",
-                ["l"] = "actions.select",
-                ["h"] = "actions.parent",
-                ["g."] = "actions.toggle_hidden",
-                ["<CR>"] = "actions.select",
-                ["<C-t>"] = "actions.select_tab",
-                ["<C-c>"] = "actions.close",
-                ["<C-l>"] = "actions.refresh",
-                ["<C-v>"] = "actions.preview",
-                ["_"] = "actions.open_cwd",
-                ["<c-<CR>>"] = "actions.cd",
-            },
-            use_default_keymaps = false,
-            default_file_explorer = true,
-        },
-        keys = { "<a-e>", "<cmd>Oil<CR>", mode = "n", desc = "Open Oil" },
-        -- Optional dependencies
+        lazy = false,
+        config = function()
+            require("oil").setup {
+                keymaps = {
+                    ["g?"] = "actions.show_help",
+                    ["l"] = "actions.select",
+                    ["h"] = "actions.parent",
+                    ["g."] = "actions.toggle_hidden",
+                    ["<CR>"] = "actions.select",
+                    ["<C-t>"] = "actions.select_tab",
+                    ["<C-c>"] = "actions.close",
+                    ["<C-l>"] = "actions.refresh",
+                    ["<C-v>"] = "actions.preview",
+                    ["_"] = "actions.open_cwd",
+                    ["<c-<CR>>"] = "actions.cd",
+                },
+                use_default_keymaps = false,
+                default_file_explorer = true,
+            }
+            local keymap = require "je12emy.utils.map"
+            keymap.nmap('<a-e>', '<cmd>Oil<CR>', { desc = "Open Oil" })
+            -- Disable netrw
+            vim.g.loaded_netrw = 1
+            vim.g.loaded_netrwPlugin = 1
+        end,
         dependencies = { "nvim-tree/nvim-web-devicons" },
     }
 }
