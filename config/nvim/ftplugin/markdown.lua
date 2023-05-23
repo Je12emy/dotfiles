5b9146e43622dc9c31da7d293e66b4e1bfa6891e
Jeremy <jeremy@je12emy.com>
2023-05-22 23:38:35 -0600

    feat(nvim): add telescope map for md files

--- config/nvim/ftplugin/markdown.lua
+++ config/nvim/ftplugin/markdown.lua
@@ -1,2 +1,5 @@
 local settings = require 'je12emy.utils.file-settings'
+local keymap = require "je12emy.utils.map"
+
+keymap.nmap('<Space>ts', '<cmd>Telescope spell_suggest<cr>', { desc = "View spell suggestions" })
 settings.notes(vim.opt)
