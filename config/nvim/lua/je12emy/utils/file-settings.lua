572b637629fd47b372f346c823076e660b00844f
Jeremy <jeremy@je12emy.com>
2023-05-22 20:25:07 -0600

    refactor(nvim): extract some functions into modules

---
+++ config/nvim/lua/je12emy/utils/file-settings.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+-- General purpose settings for note taking
+M.notes = function(file)
+    file.colorcolumn = '0'
+    file.linebreak = true
+    file.wrap = true
+    file.spell = true
+    file.autowriteall = true
+    file.spelllang = { 'en', 'es' }
+end
+
+return M
