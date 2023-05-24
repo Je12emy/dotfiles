2e4d92f2d3a266c0341cae1329aedd385faedb4b
Jeremy <jeremy@je12emy.com>
2023-05-23 22:03:20 -0600

    refactor(nvim): create wiki module

--- config/nvim/lua/je12emy/plugins/utils.lua
+++ config/nvim/lua/je12emy/plugins/utils.lua
@@ -65,42 +65,4 @@ return {
         keys = { { "<a-u>", "<cmd>UndotreeToggle<cr>", desc = "Toggle Undotree" } },
         cmd = "UndoTreeToggle"
     },
-    {
-        "epwalsh/obsidian.nvim",
-        lazy = true,
-        event = { "BufReadPre " .. vim.fn.expand "~" .. "/notes/**.md" },
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "hrsh7th/nvim-cmp",
-            "nvim-telescope/telescope.nvim",
-        },
-        opts = {
-            dir = "~/notes", -- no need to call 'vim.fn.expand' here
-            daily_notes = {
-                folder = "/dailies",
-            },
-            completion = {
-                nvim_cmp = true
-            },
-            disable_frontmatter = true,
-            follow_url_func = function(url)
-                vim.fn.jobstart({ "xdg-open", url }) -- linux
-            end,
-            use_advanced_uri = false,
-            open_app_foreground = true,
-            finder = "telescope.nvim",
-        },
-        config = function(_, opts)
-            require("obsidian").setup(opts)
-            -- Optional, override the 'gf' keymap to utilize Obsidian's search functionality.
-            -- see also: 'follow_url_func' config option above.
-            vim.keymap.set("n", "gf", function()
-                if require("obsidian").util.cursor_on_markdown_link() then
-                    return "<cmd>ObsidianFollowLink<CR>"
-                else
-                    return "gf"
-                end
-            end, { noremap = false, expr = true })
-        end,
-    },
 }
---
+++ config/nvim/lua/je12emy/plugins/wiki/mkdflow.lua
@@ -0,0 +1,8 @@
+return {
+    {
+        'jakewvincent/mkdnflow.nvim',
+        config = function()
+            require('mkdnflow').setup()
+        end
+    }
+}
