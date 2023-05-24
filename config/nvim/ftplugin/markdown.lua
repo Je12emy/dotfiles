d2eb18c5c527672419ef951f9487f4e091be2f3f
Jeremy <jeremy@je12emy.com>
2023-05-23 23:20:20 -0600

    feat(nvim): create util for opening notes

--- config/nvim/ftplugin/markdown.lua
+++ config/nvim/ftplugin/markdown.lua
@@ -1,5 +1,11 @@
 local settings = require 'je12emy.utils.file-settings'
 local keymap = require "je12emy.utils.map"
+local obsidian = require "je12emy.utils.obsidian"
 
+obsidian.setup {
+    vault_path = "/home/jeremy/notes"
+}
+
+keymap.nmap("<Space>oo", obsidian.open_note, { desc = "Open the current note in obsidian" })
 keymap.nmap('<Space>ts', '<cmd>Telescope spell_suggest<cr>', { desc = "View spell suggestions" })
 settings.notes(vim.opt)
