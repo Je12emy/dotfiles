ee2c0c6b34dc0ff8d9f37ce936bc4e952aa5a662
Jeremy <jeremy@je12emy.com>
2023-06-01 13:48:46 -0600

    feat(nvim): update markdown settings

--- config/nvim/lua/je12emy/utils/file-settings.lua
+++ config/nvim/lua/je12emy/utils/file-settings.lua
@@ -5,6 +5,7 @@ M.notes = function(file)
     file.colorcolumn = '0'
     file.linebreak = true
     file.wrap = true
+    file.breakindent = true
     file.spell = true
     file.autowriteall = true
     file.spelllang = { 'en', 'es' }
