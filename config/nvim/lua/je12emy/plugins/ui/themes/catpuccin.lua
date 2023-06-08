3d448e5a025c566d64bc688cd5ed9e91715174d8
Jeremy <jeremy@je12emy.com>
2023-06-08 15:22:45 -0600

    feat(nvim): update catpuccin theme

--- config/nvim/lua/je12emy/plugins/ui/themes/catpuccin.lua
+++ config/nvim/lua/je12emy/plugins/ui/themes/catpuccin.lua
@@ -1,19 +1,12 @@
 return {
-    {
-        "catppuccin/nvim",
-        lazy = false,
-        priority = 1000,
-        name = "catppuccin",
-        config = function()
-            vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
-            vim.cmd.colorscheme("catppuccin")
-            require("catppuccin").setup {
-                custom_highlights = function(colors)
-                    return {
-                        StatusLine = { bg = colors.none },
-                    }
-                end
-            }
-        end
-    }
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		priority = 1000,
+		name = "catppuccin",
+		config = function()
+			vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 }
