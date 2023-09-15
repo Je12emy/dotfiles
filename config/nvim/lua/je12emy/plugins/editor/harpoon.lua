66e615db4db284c1c7ee6add204795b887e9f220
Jeremy <jeremyzelaya@hotmail.es>
2023-09-15 15:05:38 -0600

    feat(nvim): new plugin harpoon

---
+++ config/nvim/lua/je12emy/plugins/editor/harpoon.lua
@@ -0,0 +1,22 @@
+return {
+    "ThePrimeagen/harpoon",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        require("telescope").load_extension('harpoon')
+    end,
+    keys = function()
+        return {
+            {
+                "<leader>m",
+                function()
+                    local mark = require('harpoon.mark')
+                    local i = mark.get_current_index()
+                    mark.toggle_file(i)
+                end,
+                desc = "Toggle grapple mark",
+            },
+            { "<leader>tm", "<cmd>Telescope harpoon marks theme=dropdown previewer=false<cr>",
+                desc = "Open grappgle marks" },
+        }
+    end
+}
