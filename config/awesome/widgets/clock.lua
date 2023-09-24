24328ca38160331a50d17cc9363fd0d5c9c72ec3
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2023-09-24 15:12:36 -0600

    fix(awesome): update textclock widget init

--- config/awesome/widgets/clock.lua
+++ config/awesome/widgets/clock.lua
@@ -4,8 +4,12 @@ local wibox = require("wibox")
 -- the widgets are initialized so the placement of the initialization is important
 local m = {}
 -- Wed Jul 19 12:46 pm
-m.standard = wibox.widget.textclock("%a %d %b, %I:%M %p")
+m.standard = function()
+	return wibox.widget.textclock("%a %d %b, %I:%M %p")
+end
 -- 2023/07/19 12:48 pm
-m.short = wibox.widget.textclock("%Y/%m/%d %H:%M %p")
+m.short = function()
+	return wibox.widget.textclock("%Y/%m/%d %H:%M %p")
+end
 
 return m
