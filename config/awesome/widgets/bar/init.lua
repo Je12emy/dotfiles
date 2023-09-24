9a4034ccc232a797c24434c610b0539784e16b1a
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2023-09-24 15:19:12 -0600

    refactor(awesome): combine wibar widgets

--- config/awesome/widgets/bar/init.lua
+++ config/awesome/widgets/bar/init.lua
@@ -1,14 +1,14 @@
 local volume_widget = require("awesome-wm-widgets.pactl-widget.volume")
 local logout_menu_widget = require("awesome-wm-widgets.logout-menu-widget.logout-menu")
-local systray_widget = require("widgets.systray")
+local systray = require("widgets.systray")
 local spacer = require("widgets.spacer")
-local topbar = require("widgets.topbar")
 local clock = require("widgets.clock")
+local bar_template = require("widgets.bar.templates")
 
 local m = {}
 
-m.standard = function(screen_taglist, screen_tasklist, screen_layout_box)
-	return topbar.standard(
+m.standard_bar = function(screen_taglist, screen_tasklist, screen_layout_box)
+	return bar_template.standard(
 		{
 			spacer.normal(),
 			screen_taglist,
@@ -16,7 +16,7 @@ m.standard = function(screen_taglist, screen_tasklist, screen_layout_box)
 		},
 		screen_tasklist,
 		{
-			systray_widget.systray,
+			systray.systray,
 			spacer.normal(),
 			screen_layout_box,
 			clock.standard(),
