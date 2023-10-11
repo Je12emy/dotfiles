0d39d3bf472d31505cbec3571b7212af092e2a49
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2023-10-11 01:19:01 -0600

    feat(awesome): edit todo.txt

--- config/nvim/lua/je12emy/keymaps.lua
+++ config/nvim/lua/je12emy/keymaps.lua
@@ -6,10 +6,9 @@ keymap.nmap('<Leader>h', ':exe "vertical resize -" .5<CR>', { desc = "Decrease s
 keymap.nmap('<Leader>l', ':exe "vertical resize +" .5<CR>', { desc = "Increase split vertical size by 5" })
 keymap.nmap('<Leader>j', ':exe "resize -" .5<CR>', { desc = "Decrease split horizontal size by 5" })
 keymap.nmap('<Leader>k', ':exe "resize +" .5<CR>', { desc = "Increase split horizontal size by 5" })
-keymap.nmap('<Leader>wr', '<C-w>=<CR>', { desc = "Restore split size" })
-keymap.nmap('<a-}>', '<cmd>bnext<CR>', { desc = "Go to next buffer" })
-keymap.nmap('<a-{>', '<cmd>bprev<CR>', { desc = "Go to previous buffer" })
-keymap.nmap('<a-x>', '<cmd>bdelete<CR>', { desc = "Close buffer" })
 keymap.nmap('<Leader>tn', '<cmd>tabnew<CR>', { desc = "Create new tab" })
 keymap.nmap('<Leader>tx', '<cmd>tabclose<CR>', { desc = "Close tab" })
 keymap.nmap('<F8>', ':setlocal spell!<CR>', { desc = "Toggle spell check" })
+
+local todo_file = os.getenv("todo_path") or "~/todo.txt"
+keymap.nmap('<a-t>', ':tab e' .. todo_file .. '<CR>', { desc = "Edit todo list" })
