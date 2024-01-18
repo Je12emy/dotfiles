ad903a5801f37554d73bfe87a73a22f4feee42fd
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2024-01-18 14:32:48 -0600

    feat(misc): add screenshoting tools
    Adds a screenshot tool, binds it in hyprland and update the install
    script for dotbot

---
+++ scripts/screenshot.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+# A simple tofi script to take a screenshot with grimshot
+
+get_output() {
+	destination=$(printf "%s\n" \
+	"Save" \
+	"Copy" \
+	| tofi --prompt="Output: ")
+
+	case $destination in
+		"Save")
+			destination="save"
+			;;
+		"Copy")
+			destination="copy"
+			;;
+		*)
+			destination="copy"
+			;;
+	esac
+}
+
+take_screenshot() {
+	grimshot --notify $1 $2 | xargs feh
+}
+
+source=$(printf '%s\n' \
+	"Full Screen Capture" \
+	"Area Capture" \
+	| tofi --horizontal=false --prompt="Screenshot: ")
+
+get_output
+
+case "$source" in
+	"Full Screen Capture")
+		take_screenshot "$destination"  "screen"
+	;;
+	"Area Capture")
+		take_screenshot "$destination" "area"
+	;;
+	*)
+		take_screenshot "$destination" "area"
+	;;
+esac
