efcfd8bae3e4f3a3f86618bf51ae4684011dea1f
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2024-01-25 15:15:15 -0600

    feat(nvim): update diffview config

--- config/nvim/lua/je12emy/plugins/editor/git/diffview.lua
+++ config/nvim/lua/je12emy/plugins/editor/git/diffview.lua
@@ -5,18 +5,5 @@ return {
 		requires = "nvim-tree/nvim-web-devicons",
 		lazy = true,
 		cmd = "DiffviewOpen",
-		keys = {
-			{ "<leader>gdo", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
-			{
-				"<leader>gdr",
-				"<cmd>DiffviewRefresh<cr>",
-				desc = "Refresh Diffview",
-			},
-			{
-				"<leader>gdf",
-				"<cmd>DiffviewToggleFiles<cr>",
-				desc = "Toggle Diffview Files",
-			},
-		},
 	},
 }
