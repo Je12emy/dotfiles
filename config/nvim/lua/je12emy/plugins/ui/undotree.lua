d355820d9d2412ae0ab9b480e5e1031877a6cb15
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2024-01-28 22:48:58 -0600

    feat(nvim): remove unsued plugin

--- config/nvim/lua/je12emy/plugins/ui/undotree.lua
+++
@@ -1,9 +0,0 @@
-return {
-	{
-		"mbbill/undotree",
-		event = { "BufReadPost", "BufNewFile" },
-		keys = { { "<a-u>", "<cmd>UndotreeToggle<cr>", desc = "Toggle Undotree" } },
-		cmd = "UndoTreeToggle",
-		enabled = false
-	},
-}
