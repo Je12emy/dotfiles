3b3955cac6c7116971601869e501c1c0182dd5f2
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2024-02-13 07:18:09 -0600

    feat(wal): update wall-set.sh script
    Copy generated templates to each config location

---
+++ config/wal/templates/colors.lua
@@ -0,0 +1,8 @@
+local M = {{}}
+
+M.background_color = "{background}"
+M.text_color = "{foreground}"
+M.color1 = "{color1}"
+M.color2 = "{color2}"
+
+return M
--- scripts/wal-set.sh
+++ scripts/wal-set.sh
@@ -2,18 +2,16 @@
 wallpaper=$1
 # Set colors with wal
 wal -i "$wallpaper"
-# Generate zellij colorscheme
-$(generate-zellij-theme.sh)
-# Append wallpaper to hyprpaper.conf
-# read it from .cache/wal
-wallpaper_path=$(cat ~/.cache/wal/wal)
-hyprpaper_path="$HOME/.config/hypr/hyprpaper.conf"
-# Delete the contents hyprpaper and generate a new config file
-$(sed -i '2,3s/.*/ /' "$hyprpaper_path")
-echo "" > $hyprpaper_path
-echo "splash = true" >> $hyprpaper_path
-echo "preload = $wallpaper_path" >> $hyprpaper_path
-echo "wallpaper = HDMI-A-1, $wallpaper_path" >> $hyprpaper_path
+
+cache_path="$HOME/.cache/wal"
+config_dir_path="$HOME/.config"
+
+# Copy themes
+cp "$cache_path/hyprpaper.conf" "$config_dir_path/hypr" 
+cp "$cache_path/pywal.kdl" "$config_dir_path/zellij" 
+cp "$cache_path/colors.lua" "$config_dir_path/wezterm" 
+cp "$cache_path/modules.jsonc" "$config_dir_path/waybar" 
+
 # Reload hyprpaper and waybar
 killall waybar
 killall hyprpaper
