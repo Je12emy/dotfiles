dc912464bae4850159103113cc680179751ba886
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2024-02-19 15:52:07 -0600

    feat(nvim): add missing file

---
+++ config/nvim/lua/je12emy/utils/env.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+M.getOS = function()
+  local osname
+  -- ask LuaJIT first
+  if jit then
+    return jit.os
+  end
+
+  -- Unix, Linux variants
+  local fh, _ = assert(io.popen("uname -o 2>/dev/null", "r"))
+  if fh then
+    osname = fh:read()
+  end
+
+  return osname or "Windows"
+end
+
+return M
