84fae4a0cc42dee86325c4a5801f85dd78e2670d
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2024-02-28 09:12:19 -0600

    refactor(scripts): remove switch statement from screen temp script

--- scripts/screen-temp.sh
+++ scripts/screen-temp.sh
@@ -4,14 +4,6 @@ declare -A screen_temperature_profiles=( ["Day"]=6500 ["Night"]=3800 )
 
 choice=$(printf "%s\n" "${!screen_temperature_profiles[@]}"  | tofi --prompt-text="Color Profiles" --horizontal=false)
 
-case "$choice" in
-	"Day")
-		$(busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q "${screen_temperature_profiles["Day"]}")
-		;;
-	"Night")
-		$(busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q "${screen_temperature_profiles["Night"]}")
-		;;
-	*)
-		$(busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q "${screen_temperature_profiles["Day"]}")
-		;;
-esac
+$(busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q "${screen_temperature_profiles["$choice"]}")
+
+notify-send "Screen temperature profile set to ${choice}"
