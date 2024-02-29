c5a7890b0acfc8cb65c29d010595adfb3dd5397b
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2024-02-28 22:08:37 -0600

    feat(scripts): conditional for wezterm

--- scripts/find-note.sh
+++ scripts/find-note.sh
@@ -1,15 +1,24 @@
 #! /bin/bash
 
-entries=$(find "$HOME/Sync/Notes" -iname "*.md")
+entries=$(find $NOTES_DIR -iname "*.md")
 
 choice=$(printf '%s\n' "${entries[@]}" \
         | cut -d '/' -f4- \
         | sort -g \
-        | tofi --prompt-text="Note: " --horizontal=false) || exit 1
+        | $MENU --prompt-text="Note: " --horizontal=false) || exit 1
 
-[[ -n choice ]] || exit
+[[ -n $choice ]] || exit
 
-# When I specify a workspace, a new instance is launched
-# wezterm cli spawn --workspace notes --domain-name local --new-window nvim "$choice"
-wezterm cli spawn --domain-name local nvim "$choice"
-notify-send "Opened note in wezterm workspace"
+if [[ $TERMINAL == "wezterm" ]];then
+        # Non zero code is returned if there are not clients
+        wezterm cli list-clients
+        if [[ $? -eq 0 ]]; then
+                wezterm cli spawn --domain-name local $EDITOR $choice
+                # When I specify a workspace, a new instance is launched
+                # wezterm cli spawn --workspace notes --domain-name local --new-window nvim "$choice"
+                notify-send "Opened note $EDITOR"
+        fi
+else
+        $TERMINAL $EDITOR $choice
+        notify-send "Opened note $EDITOR"
+fi
