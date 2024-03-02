8a6771354d7b8dfd7ce809febf26030bd8b9b99e
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2024-03-02 11:47:15 -0600

    refactr(nvim): reorganize dap config

--- config/nvim/lua/je12emy/plugins/dap.lua
+++ config/nvim/lua/je12emy/plugins/dap.lua
@@ -1,84 +1,87 @@
 return {
-    {
-        "rcarriga/nvim-dap-ui",
-        config = function()
-            local dap = require("dap")
-            local dapui = require("dapui")
-            local set = require("je12emy.utils.map")
+	{
+		"mfussenegger/nvim-dap",
+		config = function()
+			local set = require("je12emy.utils.map")
+			local dap = require("dap")
 
-            -- 
-            set.nmap("<Leader>do", dapui.open, { desc = "DAP open UI" });
-            set.nmap("<Leader>dc", dapui.close, { desc = "DAP close UI" });
+			set.nmap("<Leader>d<Space>", dap.continue, { desc = "[D]ebug launch/continue" })
 
-            dapui.setup()
-            dap.listeners.before.attach.dapui_config = function()
-                dapui.open()
-            end
-            dap.listeners.before.launch.dapui_config = function()
-                dapui.open()
-            end
-            -- dap.listeners.before.event_terminated.dapui_config = function()
-            --     dapui.close()
-            -- end
-            -- dap.listeners.before.event_exited.dapui_config = function()
-            --     dapui.close()
-            -- end
-        end,
-        dependencies = {
-            "mfussenegger/nvim-dap"
-        }
-    },
-    {
-        "mfussenegger/nvim-dap",
-        config = function()
-            local set = require("je12emy.utils.map")
-            local dap = require("dap")
+			-- NOTE: See all available DAP adapters here: https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation
+			dap.adapters.gdb = {
+				type = "executable",
+				command = "gdb",
+				args = { "-i", "dap" },
+			}
 
-            set.nmap("<Leader>d<Space>", dap.continue, { desc = "DAP continue" });
-            set.nmap("<Leader>dt", dap.toggle_breakpoint, { desc = "DAP toggle breakpoint" });
-            set.nmap("<Leader>dj", dap.step_over, { desc = "DAP step over" });
-            set.nmap("<Leader>dh", dap.step_out, { desc = "DAP step out" });
-            set.nmap("<Leader>dl", dap.step_into, { desc = "DAP step into" });
-            set.nmap("<Leader>dT",
-                function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
-            set.nmap('<Leader>dh', function()
-                require('dap.ui.widgets').hover()
-            end)
+			dap.adapters.coreclr = {
+				type = "executable",
+				command = "/usr/local/netcoredbg",
+				args = { "--interpreter=vscode" },
+			}
 
-            dap.adapters.gdb = {
-                type = "executable",
-                command = "gdb",
-                args = { "-i", "dap" }
-            }
+			dap.configurations.c = {
+				{
+					name = "Launch",
+					type = "gdb",
+					request = "launch",
+					program = function()
+						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					end,
+					cwd = "${workspaceFolder}",
+				},
+			}
 
-            dap.adapters.coreclr = {
-                type = 'executable',
-                command = '/usr/local/netcoredbg',
-                args = { '--interpreter=vscode' }
-            }
+			dap.configurations.cs = {
+				{
+					type = "coreclr",
+					name = "launch - netcoredbg",
+					request = "launch",
+					program = function()
+						return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+					end,
+				},
+			}
+		end,
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+			local set = require("je12emy.utils.map")
 
-            dap.configurations.c = {
-                {
-                    name = "Launch",
-                    type = "gdb",
-                    request = "launch",
-                    program = function()
-                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-                    end,
-                    cwd = "${workspaceFolder}",
-                },
-            }
+			dapui.setup()
+			dap.listeners.before.attach.dapui_config = function()
+				set.nmap("<Leader>dt", dap.toggle_breakpoint, { desc = "[D]ebug [t]oggle breakpoint" })
+				set.nmap("<Leader>dT", function()
+					require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+				end, { desc = "[D]debug [T]oggle log point" })
 
-            dap.configurations.cs = {
-                {
-                    type = "coreclr",
-                    name = "launch - netcoredbg",
-                    request = "launch",
-                    program = function()
-                        return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
-                    end,
-                },
-            }
-        end,
-    }
+				set.nmap("<Leader>dj", dap.step_over, { desc = "[D]debug [j]step over" })
+				set.nmap("<Leader>dh", dap.step_out, { desc = "[D]debug [h]step out" })
+				set.nmap("<Leader>dl", dap.step_into, { desc = "[D]debug [l]step into" })
+
+				set.nmap("<Leader>dk", function()
+					require("dap.ui.widgets").hover()
+				end, { desc = "[D]debug [k]hover" })
+
+				set.nmap("<Leader>do", dapui.open, { desc = "[D]ebug UI [o]pen" })
+				set.nmap("<Leader>dc", dapui.close, { desc = "[D]ebug UI [c]lose" })
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+		end,
+		dependencies = {
+			"mfussenegger/nvim-dap",
+		},
+	},
 }
