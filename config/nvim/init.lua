071f9e7fdeb5f62fcde9983f5108fe2da108313f
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2024-03-28 20:51:33 -0600

    feat(nvim): update dap config
    Update overall dap keybinds and c# dap config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -665,7 +665,7 @@ require("lazy").setup({
 
 			dap.adapters.coreclr = {
 				type = "executable",
-				command = "/usr/local/netcoredbg",
+				command = "netcoredbg",
 				args = { "--interpreter=vscode" },
 			}
 
@@ -681,16 +681,62 @@ require("lazy").setup({
 				},
 			}
 
-			dap.configurations.cs = {
+			-- source: https://github.com/mfussenegger/nvim-dap/wiki/Cookbook#making-debugging-net-easier
+			vim.g.dotnet_build_project = function()
+				local default_path = vim.fn.getcwd() .. "/"
+				if vim.g["dotnet_last_proj_path"] ~= nil then
+					default_path = vim.g["dotnet_last_proj_path"]
+				end
+				local path = vim.fn.input("Path to your *proj file", default_path, "file")
+				vim.g["dotnet_last_proj_path"] = path
+				local cmd = "dotnet build -c Debug " .. path .. " > /dev/null"
+				print("")
+				print("Cmd to execute: " .. cmd)
+				local f = os.execute(cmd)
+				if f == 0 then
+					print("\nBuild: ✔️ ")
+				else
+					print("\nBuild: ❌ (code: " .. f .. ")")
+				end
+			end
+
+			vim.g.dotnet_get_dll_path = function()
+				local request = function()
+					return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+				end
+
+				if vim.g["dotnet_last_dll_path"] == nil then
+					vim.g["dotnet_last_dll_path"] = request()
+				else
+					if
+						vim.fn.confirm(
+							"Do you want to change the path to dll?\n" .. vim.g["dotnet_last_dll_path"],
+							"&yes\n&no",
+							2
+						) == 1
+					then
+						vim.g["dotnet_last_dll_path"] = request()
+					end
+				end
+
+				return vim.g["dotnet_last_dll_path"]
+			end
+
+			local config = {
 				{
 					type = "coreclr",
 					name = "launch - netcoredbg",
 					request = "launch",
 					program = function()
-						return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+						if vim.fn.confirm("Should I recompile first?", "&yes\n&no", 2) == 1 then
+							vim.g.dotnet_build_project()
+						end
+						return vim.g.dotnet_get_dll_path()
 					end,
 				},
 			}
+
+			dap.configurations.cs = config
 		end,
 	},
 	{
@@ -701,8 +747,14 @@ require("lazy").setup({
 
 			dapui.setup()
 			dap.listeners.before.attach.dapui_config = function()
-				vim.keymap.set("i", "<Leader>dt", dap.toggle_breakpoint, { desc = "[D]ebug [t]oggle breakpoint" })
-				vim.keymap.set("i", "<Leader>dT", function()
+				-- vim.notify("Attaching to debugger")
+			end
+
+			dap.listeners.before.launch.dapui_config = function()
+				vim.notify("Debugger attached")
+
+				vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, { desc = "[D]ebug [t]oggle breakpoint" })
+				vim.keymap.set("n", "<Leader>dT", function()
 					require("dap").set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
 				end, { desc = "[D]debug [T]oggle log point" })
 
@@ -715,10 +767,7 @@ require("lazy").setup({
 				end, { desc = "[D]debug [k]hover" })
 
 				vim.keymap.set("n", "<Leader>do", dapui.open, { desc = "[D]ebug UI [o]pen" })
-				vim.keymap.set("<Leader>dc", dapui.close, { desc = "[D]ebug UI [c]lose" })
-				dapui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
+				vim.keymap.set("n", "<Leader>dc", dapui.close, { desc = "[D]ebug UI [c]lose" })
 				dapui.open()
 			end
 			dap.listeners.before.event_terminated.dapui_config = function()
@@ -730,6 +779,7 @@ require("lazy").setup({
 		end,
 		dependencies = {
 			"mfussenegger/nvim-dap",
+			"nvim-neotest/nvim-nio",
 		},
 	},
 	{
