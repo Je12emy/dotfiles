bf47c014481e1c4d20105b137a91603b4719544f
Je12emy <jeremyzelaya@hotmail.es>
2024-09-19 08:13:34 -0600

    feat(nvim): remove whichkey

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -350,22 +350,6 @@ require("lazy").setup({
 			end, { desc = "[s]earch harpoon [m]arks" })
 		end,
 	},
-	{ -- Useful plugin to show you pending keybinds.
-		"folke/which-key.nvim",
-		event = "VimEnter", -- Sets the loading event to 'VimEnter'
-		config = function() -- This is the function that runs, AFTER loading
-			require("which-key").setup()
-
-			-- Document existing key chains
-			-- require("which-key").register({
-			-- 	["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-			-- 	["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-			-- 	["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-			-- 	["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-			-- 	["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-			-- })
-		end,
-	},
 	{
 		-- Git signs in the column + other Git utils
 		"lewis6991/gitsigns.nvim",
