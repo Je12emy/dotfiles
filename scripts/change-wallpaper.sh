990304a8a4ef32eb7bcba25452fb45f70ae092f4
Je12emy <jeremyzelaya@hotmail.es>
2024-09-30 14:40:22 -0600

    feat(hypr): set greet text in hyprlock

--- scripts/change-wallpaper.sh
+++ scripts/change-wallpaper.sh
@@ -16,12 +16,19 @@ hyprlock_config="$hypr_base/hyprlock.conf"
 
 wallpaper_path="$wallpapers_dir/$wallpaper"
 
+
 # Replace the config variables in hyprpaper
 sed -i '/wallpaper =/c\wallpaper = ,'"$wallpaper_path"'' $hyprpaper_config
 sed -i '/preload =/c\preload = '"$wallpaper_path"'' $hyprpaper_config
 
 # Replace the wallpaper variable in hyprlock
 sed -i '/path =/c\path = '"$wallpaper_path"'' $hyprlock_config
+
+# Fortune outputs a quote, if available set the lock message
+if command -v fortune >&2; then
+	greet=$(fortune -s -a)
+	sed -i '/text =/c\text = '"$greet"'' $hyprlock_config
+fi
 
 # Restart hyprpaper
 killall hyprpaper
