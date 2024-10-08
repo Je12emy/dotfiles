52663914b026b500c1b5470c6ec9369401ee7c8f
Je12emy <jeremyzelaya@hotmail.es>
2024-10-07 22:32:56 -0600

    feat(wezterm): integrate with resurrect

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -310,7 +310,52 @@ config.key_tables = {
 					end
 				end)
 			}
-		}
+		},
+		{
+			key = "s",
+			action = wezterm.action_callback(function(_, _)
+				resurrect.save_state(resurrect.workspace_state.get_workspace_state())
+			end),
+		},
+		{
+			key = "r",
+			action = wezterm.action_callback(function(win, pane)
+				resurrect.fuzzy_load(win, pane, function(id, label)
+					local type = string.match(id, "^([^/]+)") -- match before '/'
+					id = string.match(id, "([^/]+)$") -- match after '/'
+					id = string.match(id, "(.+)%..+$") -- remove file extention
+					local opts = {
+						relative = true,
+						restore_text = true,
+						on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+					}
+					if type == "workspace" then
+						local state = resurrect.load_state(id, "workspace")
+						resurrect.workspace_state.restore_workspace(state, opts)
+					elseif type == "window" then
+						local state = resurrect.load_state(id, "window")
+						resurrect.window_state.restore_window(pane:window(), state, opts)
+					elseif type == "tab" then
+						local state = resurrect.load_state(id, "tab")
+						resurrect.tab_state.restore_tab(pane:tab(), state, opts)
+					end
+				end)
+			end),
+		},
+		{
+			key = "d",
+			action = wezterm.action_callback(function(win, pane)
+				resurrect.fuzzy_load(win, pane, function(id)
+						resurrect.delete_state(id)
+					end,
+					{
+						title = "Delete State",
+						description = "Select State to Delete and press Enter = accept, Esc = cancel, / = filter",
+						fuzzy_description = "Search State to Delete: ",
+						is_fuzzy = true,
+					})
+			end),
+		},
 	})
 }
 -- Root keybinds
@@ -393,50 +438,13 @@ config.keys = {
 		key = "-",
 		action = wezterm.action.DecreaseFontSize,
 	},
-	{
-		mods = "LEADER",
-		key = "z",
-		action = wezterm.action_callback(function(win, pane)
-			resurrect.save_state(resurrect.workspace_state.get_workspace_state())
-		end),
-	},
-	{
-		key = "T",
-		mods = "ALT",
-		action = resurrect.tab_state.save_tab_action(),
-	},
-	{
-		mods = "ALT",
-		key = "r",
-		action = wezterm.action_callback(function(win, pane)
-			resurrect.fuzzy_load(win, pane, function(id, label)
-				local type = string.match(id, "^([^/]+)") -- match before '/'
-				id = string.match(id, "([^/]+)$") -- match after '/'
-				id = string.match(id, "(.+)%..+$") -- remove file extention
-				local opts = {
-					relative = true,
-					restore_text = true,
-					on_pane_restore = resurrect.tab_state.default_on_pane_restore,
-				}
-				if type == "workspace" then
-					local state = resurrect.load_state(id, "workspace")
-					resurrect.workspace_state.restore_workspace(state, opts)
-				elseif type == "window" then
-					local state = resurrect.load_state(id, "window")
-					resurrect.window_state.restore_window(pane:window(), state, opts)
-				elseif type == "tab" then
-					local state = resurrect.load_state(id, "tab")
-					resurrect.tab_state.restore_tab(pane:tab(), state, opts)
-				end
-			end)
-		end),
-	}
 }
 
 -- Status bar
 local icons = { "", "", "", "", "", "", "󰌽", "", "", "󰢚" }
-local random_icon = icons[math.random(0, #icons)]
+local random_icon = icons[math.random(1, #icons)]
 config.status_update_interval = 1000
+
 wezterm.on("update-right-status", function(window, _)
 	local leader = ""
 	local mode = ""
