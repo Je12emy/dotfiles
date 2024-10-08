1a94a5992d35892b17ff8f630263b1da2a8747ae
Je12emy <jeremyzelaya@hotmail.es>
2024-10-07 22:25:43 -0600

    feat(wezterm): create sessions

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -50,6 +50,9 @@ config.colors = {
 
 	}
 }
+
+-- Plugins
+local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
 -- Keybinds
 local mode_timeout = 3000
 local leader_timeout = 2000
@@ -257,6 +260,58 @@ config.key_tables = {
 			action = wezterm.action.MoveTabRelative(-1),
 		}
 	}),
+	["Workspace"] = append_escape_keys({
+
+		{ key = 'n', action = wezterm.action.SwitchToWorkspace },
+		{
+			key = "c",
+			action = wezterm.action.PromptInputLine {
+				description = wezterm.format {
+					{ Attribute = { Intensity = 'Bold' } },
+					{ Foreground = { AnsiColor = 'Fuchsia' } },
+					{ Text = 'Enter name for new workspace' },
+				},
+				action = wezterm.action_callback(function(window, pane, line)
+					-- line will be `nil` if they hit escape without entering anything
+					-- An empty string if they just hit enter
+					-- Or the actual line of text they wrote
+					if line then
+						window:perform_action(
+							wezterm.action.SwitchToWorkspace {
+								name = line,
+							},
+							pane
+						)
+					end
+				end),
+			},
+		},
+		{
+			key = "f",
+			action = wezterm.action.ShowLauncherArgs {
+				flags = 'FUZZY|WORKSPACES',
+			},
+
+		},
+		{
+			key = "r",
+			action = wezterm.action.PromptInputLine {
+				description = wezterm.format {
+					{ Attribute = { Intensity = 'Bold' } },
+					{ Foreground = { AnsiColor = 'Fuchsia' } },
+					{ Text = 'Enter a new name for current workspace' },
+				},
+				action = wezterm.action_callback(function(_, _, line)
+					if line then
+						wezterm.mux.rename_workspace(
+							wezterm.mux.get_active_workspace(),
+							line
+						)
+					end
+				end)
+			}
+		}
+	})
 }
 -- Root keybinds
 config.keys = {
@@ -305,6 +360,11 @@ config.keys = {
 	},
 	{
 		mods = "LEADER",
+		key = "w",
+		action = wezterm.action.ActivateKeyTable({ name = "Workspace", one_shot = true }),
+	},
+	{
+		mods = "LEADER",
 		key = "Space",
 		action = wezterm.action.QuickSelect,
 	},
@@ -333,9 +393,49 @@ config.keys = {
 		key = "-",
 		action = wezterm.action.DecreaseFontSize,
 	},
+	{
+		mods = "LEADER",
+		key = "z",
+		action = wezterm.action_callback(function(win, pane)
+			resurrect.save_state(resurrect.workspace_state.get_workspace_state())
+		end),
+	},
+	{
+		key = "T",
+		mods = "ALT",
+		action = resurrect.tab_state.save_tab_action(),
+	},
+	{
+		mods = "ALT",
+		key = "r",
+		action = wezterm.action_callback(function(win, pane)
+			resurrect.fuzzy_load(win, pane, function(id, label)
+				local type = string.match(id, "^([^/]+)") -- match before '/'
+				id = string.match(id, "([^/]+)$") -- match after '/'
+				id = string.match(id, "(.+)%..+$") -- remove file extention
+				local opts = {
+					relative = true,
+					restore_text = true,
+					on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+				}
+				if type == "workspace" then
+					local state = resurrect.load_state(id, "workspace")
+					resurrect.workspace_state.restore_workspace(state, opts)
+				elseif type == "window" then
+					local state = resurrect.load_state(id, "window")
+					resurrect.window_state.restore_window(pane:window(), state, opts)
+				elseif type == "tab" then
+					local state = resurrect.load_state(id, "tab")
+					resurrect.tab_state.restore_tab(pane:tab(), state, opts)
+				end
+			end)
+		end),
+	}
 }
 
 -- Status bar
+local icons = { "", "", "", "", "", "", "󰌽", "", "", "󰢚" }
+local random_icon = icons[math.random(0, #icons)]
 config.status_update_interval = 1000
 wezterm.on("update-right-status", function(window, _)
 	local leader = ""
@@ -354,10 +454,21 @@ wezterm.on("update-right-status", function(window, _)
 		end
 		mode = "▶ " .. key_table .. " "
 	end
+
+	local active_workspace = string.upper(window:active_workspace())
+	if active_workspace == "DEFAULT" then
+		active_workspace = ""
+	else
+		active_workspace = " " .. random_icon .. " " .. active_workspace
+	end
+
 	-- Display the content
 	window:set_right_status(wezterm.format({
 		{ Text = leader },
 		{ Text = mode },
+		{ Foreground = { Color = theme.background } },
+		{ Background = { Color = random_theme_accent } },
+		{ Text = active_workspace },
 	}))
 end)
 
