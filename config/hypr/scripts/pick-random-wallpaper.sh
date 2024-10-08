60537e49b7d98ad46564fb584661e14326411344
Je12emy <jeremyzelaya@hotmail.es>
2024-10-07 19:53:29 -0600

    feat(hypr): change wallpaper with script

--- config/hypr/scripts/pick-random-wallpaper.sh
+++ config/hypr/scripts/pick-random-wallpaper.sh
@@ -1,6 +1,19 @@
 #! /bin/bash
 # Picks a random wallpaper and set it using hyprctl
+
+# Pick a random wallpaper
 wallpaper_dir="$HOME/Pictures/wallpapers"
-wallpaper=$(ls $wallpaper_dir | shuf -n 1)
+wallpaper=$(ls $wallpaper_dir/*.png | shuf -n 1)
+echo $wallpaper_path
+target_file="$HOME/wallpaper.png"
+
+# Create a symlink to the wallpaper, if it exists, remove it
+if [ -f "$target_file" ]; then
+	echo "Removing old symlink"
+	rm "$target_file"
+fi
+ln -s "$wallpaper" "$target_file"
+
+# Set the wallpaper at runtime by using hyprctl
 hyprctl hyprpaper preload "$wallpaper"
 hyprctl hyprpaper wallpaper ",$wallpaper"
