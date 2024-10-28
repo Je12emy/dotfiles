c3b8ddca7c15d22878dca7a2d636768d04022bbb
Je12emy <jeremyzelaya@hotmail.es>
2024-10-27 22:01:12 -0600

    feat(nvim): disable cmp

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -562,6 +562,7 @@ require("lazy").setup({
 		-- Completition engine
 		"hrsh7th/nvim-cmp",
 		event = "InsertEnter",
+		enabled = false,
 		config = function()
 			local cmp = require("cmp")
 			cmp.setup({
@@ -604,11 +605,42 @@ require("lazy").setup({
 		"Decodetalkers/csharpls-extended-lsp.nvim",
 	},
 	{
+		'saghen/blink.cmp',
+		lazy = false, -- lazy loading handled internally
+		-- optional: provides snippets for the snippet source
+		dependencies = 'rafamadriz/friendly-snippets',
+
+		-- use a release tag to download pre-built binaries
+		version = 'v0.*',
+		-- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+		-- build = 'cargo build --release',
+		-- On musl libc based systems you need to add this flag
+		-- build = 'RUSTFLAGS="-C target-feature=-crt-static" cargo build --release',
+
+		opts = {
+			highlight = {
+				-- sets the fallback highlight groups to nvim-cmp's highlight groups
+				-- useful for when your theme doesn't support blink.cmp
+				-- will be removed in a future release, assuming themes add support
+				use_nvim_cmp_as_default = false,
+			},
+			-- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+			-- adjusts spacing to ensure icons are aligned
+			nerd_font_variant = 'normal',
+
+			-- experimental auto-brackets support
+			-- accept = { auto_brackets = { enabled = true } }
+
+			-- experimental signature help support
+			-- trigger = { signature_help = { enabled = true } }
+		}
+	},
+	{
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			capabilities.textDocument.completion.completionItem.snippetSupport = true
+			-- local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			-- capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
