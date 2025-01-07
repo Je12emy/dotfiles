dcf386414d960a0a5796f6aa51fba418a9b4ed0a
Je12emy <jeremyzelaya@hotmail.es>
2025-01-07 09:32:54 -0600

    feat(nvim): dim inactive panes

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -500,6 +500,11 @@ require("lazy").setup({
 		config = function()
 			require("catppuccin").setup({
 				flavour = "mocha", -- latte, frappe, macchiato, mocha
+				dim_inactive = {
+					enabled = true, -- dims the background color of inactive window
+					shade = "dark",
+					percentage = 0.15, -- percentage of the shade to apply to the inactive window
+				},
 			})
 			vim.cmd.colorscheme("catppuccin")
 		end
